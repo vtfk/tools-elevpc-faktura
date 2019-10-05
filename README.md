77402904e159cf0bbfbcc971c6693b8f67eb9f94
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-10-05 08:13:01 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # tools-elevpc-faktura
+
 All you need to generate invoices
+
+# Usasage
+
+```
+$ npm run convert
+```
+
+# License
+
+[MIT](LICENSE)
---
+++ utils/convert-elever.js
@@ -0,0 +1,10 @@
+(async () => {
+  const { writeFile } = require('fs').promises
+  const csv = require('csvtojson')
+  const logger = require('../lib/logger')
+  const data = await csv().fromFile('data/elever.csv')
+  logger('info', ['tools', 'convert-elever', 'got', data.length, 'elever'])
+  await writeFile('data/elever.json', JSON.stringify(data, null, 2), 'utf-8')
+  logger('info', ['tools', 'convert-elever', 'file saved'])
+  logger('info', ['tools', 'convert-elever', 'finished'])
+})()
