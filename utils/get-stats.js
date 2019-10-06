7d3a27643914c4b8074b9ade8c001b1ae02bece8
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-10-06 18:18:25 +0200

    Updates stats

--- utils/get-stats.js
+++ utils/get-stats.js
@@ -1,9 +1,13 @@
 (async () => {
   const logger = require('../lib/logger')
   const elever = require('../data/elever-with-invoice-uid.json')
+  const errors = require('../data/errors.json')
+  const dsf = require('../data/elever-with-dsf-data.json')
   logger('info', ['utils', 'get-stats', 'got', elever.length, 'students'])
   const nouid = elever.filter(elev => elev.invoiceUid === false)
   const selfuid = elever.filter(elev => elev.fnr === elev.invoiceUid)
   logger('info', ['utils', 'get-stats', 'got', nouid.length, 'students without invoiceUid'])
   logger('info', ['utils', 'get-stats', 'got', selfuid.length, 'students with theireselves as invoiceUid'])
+  logger('info', ['utils', 'get-stats', 'got', dsf.length, 'students with dsf data'])
+  logger('info', ['utils', 'get-stats', 'got', errors.length, 'students with errors'])
 })()
