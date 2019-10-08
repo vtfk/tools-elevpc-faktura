e90344a3322924054400d967a29a69968c7feee2
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-10-08 08:15:55 +0200

    Adds write data to every loop (patch)

--- utils/add-invoice-uid.js
+++ utils/add-invoice-uid.js
@@ -2,14 +2,30 @@
   require('dotenv').config()
   const { writeFile } = require('fs').promises
   const getInvoiceUID = require('../lib/get-invoice-uid')
-  const elever = require('../data/elever.json')
   const logger = require('../lib/logger')
-  const data = []
+  let data = []
+  let elever = []
+  try {
+    const requiredData = require('../data/elever-with-invoice-uid.json')
+    data = requiredData
+    logger('info', ['utils', 'add-invoice-uid', 'got', 'data'])
+  } catch (error) {
+    logger('warn', ['utils', 'add-invoice-uid', 'no data found'])
+  }
+  try {
+    const requiredElevData = require('../data/elever-copy.json')
+    elever = requiredElevData
+    logger('info', ['utils', 'add-invoice-uid', 'got', 'students'])
+  } catch (error) {
+    logger('warn', ['utils', 'add-invoice-uid', 'no students found'])
+  }
   while (elever.length > 0) {
     logger('info', ['utils', 'add-invoice-uid', 'got', elever.length, 'students left'])
     const student = elever.pop()
     const invoiceUid = await getInvoiceUID(student.fnr)
     data.push({ ...student, invoiceUid })
+    await writeFile('data/elever-with-invoice-uid.json', JSON.stringify(data, null, 2), 'utf-8')
+    await writeFile('data/elever-copy.json', JSON.stringify(elever, null, 2), 'utf-8')
   }
   logger('info', ['utils', 'add-invoice-uid', 'got', data.length, 'students with invoice uid'])
   await writeFile('data/elever-with-invoice-uid.json', JSON.stringify(data, null, 2), 'utf-8')
--- utils/convert-elever.js
+++ utils/convert-elever.js
@@ -5,6 +5,7 @@
   const data = await csv().fromFile('data/elever.csv')
   logger('info', ['tools', 'convert-elever', 'got', data.length, 'elever'])
   await writeFile('data/elever.json', JSON.stringify(data, null, 2), 'utf-8')
+  await writeFile('data/elever-copy.json', JSON.stringify(data, null, 2), 'utf-8')
   logger('info', ['tools', 'convert-elever', 'file saved'])
   logger('info', ['tools', 'convert-elever', 'finished'])
 })()
